8f241532a09f6e9ed1d588e67910094e2224ec0f
suryasureshp20-dev <suryasuresh.p20@gmail.com>
2025-08-01 17:41:29 +0530

    fix: set ConfigDir for OV config route

--- ui/routers/router.go
+++ ui/routers/router.go
@@ -21,7 +21,7 @@ func Init(configDir string) {
 	web.Router("/auth/google/callback", &controllers.LoginController{}, "get:GoogleCallback")	
 	web.Router("/profile", &controllers.ProfileController{})
 	web.Router("/settings", &controllers.SettingsController{})
-	web.Router("/ov/config", &controllers.OVConfigController{})
+       web.Router("/ov/config", &controllers.OVConfigController{ConfigDir: configDir})
 	web.Router("/logs", &controllers.LogsController{})
 	web.Router("/ov/clientconfig", &controllers.OVClientConfigController{ConfigDir: configDir})
 	web.Router("/easyrsa/config", &controllers.EasyRSAConfigController{ConfigDir: configDir})
